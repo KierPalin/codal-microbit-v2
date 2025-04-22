12a1b64f301c39686fa44138fdc391bc77b07b15
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-04-22 08:37:31 +0100

    Trying to rectify spurious no matching function for call error 2

--- model/MicroBitIO.cpp
+++ model/MicroBitIO.cpp
@@ -86,8 +86,8 @@ MicroBitIO::MicroBitIO(NRF52ADC &a, TouchSensor &s) :
     //WDS
     // P21(ID_PIN_P44, P0_00, PIN_CAPABILITY_AD),
     // P22(ID_PIN_P45, P0_20, PIN_CAPABILITY_AD),
-    P23(ID_PIN_P46, P0_29, PIN_CAPABILITY_AD),
-    P24(ID_PIN_P47, P0_31, PIN_CAPABILITY_AD),
+    // P23(ID_PIN_P46, P0_29, PIN_CAPABILITY_AD),
+    // P24(ID_PIN_P47, P0_31, PIN_CAPABILITY_AD),
 
     // Aliases
     // col1(P4),
@@ -108,8 +108,8 @@ MicroBitIO::MicroBitIO(NRF52ADC &a, TouchSensor &s) :
 
     // P21.setPull(PullMode::None);
     // P22.setPull(PullMode::None);
-    P23.setPull(PullMode::None);
-    P24.setPull(PullMode::None);
+    // P23.setPull(PullMode::None);
+    // P24.setPull(PullMode::None);
 
     // Ensure all internal multiplexed pins are configured with no pull resistors.
     // col1.setPull(PullMode::None);
