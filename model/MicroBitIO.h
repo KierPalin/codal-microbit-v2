6bc22d7d6297e315efa6e502ff70a67af647562f
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-04-22 15:04:10 +0100

    testing renabling P23

--- model/MicroBitIO.h
+++ model/MicroBitIO.h
@@ -388,7 +388,7 @@ namespace codal
             // WDS:
             NRF52Pin          P21;         
             // NRF52Pin          P22;         
-            // NRF52Pin          P23;         
+            NRF52Pin          P23;         
             // NRF52Pin          P24;
 
             // Internal Pins
